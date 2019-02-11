e7c624835bfc7acf4385d896d7b3ae7c4ac04fb7
Дмитрий Золотов <zolotov.dmitriy.a@gmail.com>
2019-02-11 12:51:51 +0300

    Задание №4: Сделать всё то же самое, но для контактов

---
+++ addressbook_web_tests/addressbook_web_tests/ContactForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_tests
+{
+    class ContactForm
+    {
+        private string firstnameput;
+        private string lastnameput = "";
+
+
+
+        public ContactForm(string firstnameput)
+        {
+            this.firstnameput = firstnameput;
+        }
+
+               
+        public string Firstnameput
+        {
+            get
+            {
+                return firstnameput;
+            }
+            set
+            {
+                firstnameput = value;
+            }
+        }
+
+        public string Lastnameput
+        {
+            get
+            {
+                return lastnameput;
+            }
+            set
+            {
+                lastnameput = value;
+            }
+        }      
+    }
+}
